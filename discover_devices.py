f07d52359300d895d02d9d389f1edeb0b0129cf3
ThomasRadeke <rahdick@gmx.de>
2025-04-06 17:33:04 +0200

    Added simple BT scanning script.

---
+++ discover_devices.py
@@ -0,0 +1,13 @@
+"""
+Show a list of detected bluetooth devices and their MAC addresses.
+"""
+from bluetooth import *
+
+print("performing inquiry...")
+
+nearby_devices = discover_devices(lookup_names = True)
+
+print("found %d devices" % len(nearby_devices))
+
+for name, addr in nearby_devices:
+     print(" %s - %s" % (addr, name))
